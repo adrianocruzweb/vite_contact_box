28026fe4f8c8ed721b4ae33e91dd38c99dfd9a66
adrianocruzweb <adrianocruzweb@hotmail.com>
2022-05-10 14:39:03 -0400

    remove use state because i have this in popover

--- src/components/Widget.tsx
+++ src/components/Widget.tsx
@@ -1,5 +1,24 @@
-import {ChatTeardropDots} from 'phosphor-react';
+import {ChatTeardropDots} from 'phosphor-react'
+import { useState } from 'react'
+import { Popover } from '@headlessui/react'
 
 export function Widget(){
-    return <h1><ChatTeardropDots size="200" weight="fill"/></h1>
+    const [isWidgetOpen, setIsWidgetOpen] = useState(false)
+
+    function toggleWidgetVisibility(){
+        setIsWidgetOpen(!isWidgetOpen)
+    }
+
+    return (
+        <div className="absolute bottom-5 right-5">
+            {isWidgetOpen && <p>Hello Galera</p>}
+            <button onClick={toggleWidgetVisibility} className="bg-brand-500 rounded-full px-3 h-12 text-white flex items-center group">
+                <ChatTeardropDots className="w-6 h-6"/>
+                <span className="max-width-0 overflow-hidden group-hover:max-w-xs transition-all duration-500 ease-linear">
+                    <span className="pl-2"></span>
+                    Feedback
+                </span>
+            </button>
+        </div>
+    )
 }
