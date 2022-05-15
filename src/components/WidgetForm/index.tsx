0d97346d7543d72c91dc1dc8993e0d6b3e5b348a
adrianocruzweb <adrianocruzweb@hotmail.com>
2022-05-15 10:49:13 -0400

    insert button return back

--- src/components/WidgetForm/index.tsx
+++ src/components/WidgetForm/index.tsx
@@ -37,12 +37,20 @@ export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetForm(){
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null);
+
+    function handleRestartFeedback(){
+        setFeedbackType(null);
+    }
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
             {!feedbackType ? (
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType}/>
             ):(
-                <FeedbackContentStep feedbackType={feedbackType}/>
+                <FeedbackContentStep
+                    feedbackType={feedbackType}
+                    onFeedbackRestartRequested={handleRestartFeedback}
+                />
             )}
             <footer className="text-xs text-neutral-400">
                 Feito com ♥ pela <a className="underline underline-offset-2" href="http://rocketseta.com.br">RocketSeat</a>
